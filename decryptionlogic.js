e0aff8f6cecead44a5dc533004d3cf6067ebfa71
BalazsManus <contact@balazsmanus.hu>
2024-03-02 14:45:08 +0100

    prevent memory leak, remove console filling crap

--- decryptionlogic.js
+++ decryptionlogic.js
@@ -15,15 +15,19 @@ async function decrypt() {
     content = content.substring(prefixToRemove.length, content.length);
     content = await fromBase64(content);
     content = await UTF8GetString(content);
-    console.log('Final decrypted string is: ' + content);
     document.getElementById('Output').value = content;
+    // Delete content from variables, to prevent memory leaks
+    content = '';
+    key = '';
 }
 async function TRIPLEDESdecrypt(str, key) {
+    console.log('3DES decrypting...')
     let out = CryptoJS.TripleDES.decrypt(str, key);
-    console.log('3DES decrypted: ' + out.toString(CryptoJS.enc.Utf8));
+    console.log('3DES decrypted, length: ' + out.toString(CryptoJS.enc.Utf8).length);
     return out.toString(CryptoJS.enc.Utf8);
 }
 async function decompress(str) {
+    console.log('Decompressing...')
     return new Promise((resolve, reject) => {
         const data = str.split('').map(function (e) {
             return e.charCodeAt(0);
@@ -35,30 +39,34 @@ async function decompress(str) {
             decompressedStr += String.fromCharCode.apply(null, chunk);
         }
         resolve(decompressedStr);
-        console.log('Decompressed string is: ' + decompressedStr);
+        console.log('Decompressed, length: ' + decompressedStr.length);
         return decompressedStr;
     });
 }
 async function DESdecrypt(str, key) {
+    console.log('DES decrypting...')
     let out = CryptoJS.DES.decrypt(str, key);
-    console.log('DES decrypted: ' + out.toString(CryptoJS.enc.Utf8));
+    console.log('DES decrypted, length: ' + out.toString(CryptoJS.enc.Utf8).length);
     return out.toString(CryptoJS.enc.Utf8);
 }
 async function AESdecrypt(str, key) {
+    console.log('AES decrypting...')
     let out = CryptoJS.AES.decrypt(str, key);
-    console.log('AES decrypted: ' + out.toString(CryptoJS.enc.Utf8));
+    console.log('AES decrypted, length: ' + out.toString(CryptoJS.enc.Utf8).length);
     return out.toString(CryptoJS.enc.Utf8);
 }
 async function fromBase64(str) {
+    console.log('Base64 is processing...')
     let out = atob(str);
-    console.log('String is: ' + out.toString());
+    console.log('Base64 decrypted, length: ' + out.toString().length);
     return out.toString();
 }
 async function UTF8GetString(str) {
+    console.log('UTF8 is processing...')
     let numbers = str.split(',').map(Number);
     let uint8Array = new Uint8Array(numbers);
     let out = await textDecoder.decode(uint8Array);
-    console.log('String is: ' + out.toString());
+    console.log('UTF8 encoded, length ' + out.toString().length);
     return out.toString();
 }
 
--- encryptionlogic.js
+++ encryptionlogic.js
@@ -14,25 +14,31 @@ async function encrypt() {
     content = await DESencrypt(content, key);
     content = await compress(content);
     content = await TRIPLEDESencrypt(content, key);
-    console.log('Final encrypted string is: ' + content);
     document.getElementById('Output').value = content;
+    // Delete content from variables, to prevent memory leaks
+    content = '';
+    key = '';
 }
 async function UTF8GetBytes(str) {
+    console.log('UTF8 is processing...')
     let out = await textEncoder.encode(str);
-    console.log('UTF8 String bytes are: ' + out.toString());
+    console.log('UTF8 encoded, length ' + out.toString().length);
     return out.toString();
 }
 async function toBase64(str) {
+    console.log('Base64 is processing...')
     let out = await btoa(str);
-    console.log('Base64 String is: ' + out.toString());
+    console.log('Base64 encrypted, length ' + out.toString().length);
     return out.toString();
 }
 async function AESencrypt(str, key) {
+    console.log('AES is processing...')
     let out = await CryptoJS.AES.encrypt(str, key);
-    console.log('AES encrypted: ' + out.toString());
+    console.log('AES encrypted, length: ' + out.toString().length);
     return out.toString();
 }
 async function compress(str) {
+    console.log('Compressing...')
     return new Promise((resolve, reject) => {
         const encoder = new TextEncoder();
         const data = encoder.encode(str);
@@ -47,18 +53,20 @@ async function compress(str) {
             compressedStr += String.fromCharCode.apply(null, chunk);
         }
         resolve(compressedStr);
-        console.log('Compressed string is: ' + compressedStr.toString());
+        console.log('Compressed, length: ' + compressedStr.toString().length);
         return compressedStr.toString();
     });
 }
 async function DESencrypt(str, key) {
+    console.log('DES is processing...')
     let out = await CryptoJS.DES.encrypt(str, key);
-    console.log('DES encrypted: ' + out.toString());
+    console.log('DES encrypted, length: ' + out.toString().length);
     return out.toString();
 }
 async function TRIPLEDESencrypt(str, key) {
+    console.log('3DES is processing...')
     let out = await CryptoJS.TripleDES.encrypt(str, key);
-    console.log('3DES encrypted: ' + out.toString());
+    console.log('3DES encrypted, length: ' + out.toString().length);
     return out.toString();
 }
 
